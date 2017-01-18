ba587738c0123164cf8cbe7ac2b635f5739d9c4b
Liuchundong <liuchundong2008@gmail.com>
2017-01-18 17:20:41 +0800

    add multi-main makefile

---
+++ src/HelloAgent.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : CProject.c
+ Author      : liuchundong1
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, ANSI-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello Agent!!!"); /* prints !!!Hello Agent!!! */
+	return EXIT_SUCCESS;
+}
---
+++ src/HelloWorld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : CProject.c
+ Author      : liuchundong1
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, ANSI-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
